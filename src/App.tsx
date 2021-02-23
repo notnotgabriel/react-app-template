1bb58680467546436c1da26e9d66414ecddfe809
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2021-02-23 01:20:44 -0300

    add prettier; eslint with ts linter and editorconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 
-const App = () => {
-  return <p>App</p>;
-};
+const App = () => <p>App</p>;
 
 export default hot(App);
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+const func: Function = () => 1;
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('app')
+  document.getElementById('app'),
 );
